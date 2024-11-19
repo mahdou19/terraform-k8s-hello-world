9238b33308f957c1081d63e797d6b108828704f7
Mamadou SALL <sallmahdou1@gmail.com>
2024-11-19 20:34:34 +0100

    feat: implement and deploy cluster kubernetes

--- main.tf
+++ main.tf
@@ -1,3 +1,26 @@
+/*****************************************************
+                VPC Private Network
+******************************************************/
+
 resource "scaleway_vpc_private_network" "private_network" {
   name = "private-network-${local.name}"
+}
+
+/*****************************************************
+                Deploy Cluster K8s
+******************************************************/
+
+resource "scaleway_k8s_cluster" "cluster" {
+  name                        = "cluster-${local.name}"
+  version                     = "1.29.1"
+  cni                         = "calico"
+  private_network_id          = scaleway_vpc_private_network.private_network.id
+  delete_additional_resources = true
+}
+
+resource "scaleway_k8s_pool" "pool" {
+  cluster_id = scaleway_k8s_cluster.cluster.id
+  name       = "pool-${local.name}"
+  node_type  = "DEV1-M"
+  size       = 1
 }
